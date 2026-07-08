e3e82237a0532c56ec8e9a7fd8bce2e2e66991c5
TedjaSatedji <dewaagung670@gmail.com>
2026-07-08 13:40:27 +0700

    fix: resolve tile loading error (Map data not yet available) at high zoom levels by adding maxNativeZoom

--- app.js
+++ app.js
@@ -43,25 +43,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const layerLight = L.tileLayer(TILE_LIGHT_URL, {
     attribution: TILE_ATTRIBUTION,
     subdomains: "abcd",
-    maxZoom: 20
+    maxZoom: 20,
+    maxNativeZoom: 18
   });
 
   const layerDark = L.tileLayer(TILE_DARK_URL, {
     attribution: TILE_ATTRIBUTION,
     subdomains: "abcd",
-    maxZoom: 20
+    maxZoom: 20,
+    maxNativeZoom: 18
   });
 
   // Citra Satelit Esri + Overlay Label CartoDB
   const layerSatelliteImagery = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
     attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-    maxZoom: 20
+    maxZoom: 20,
+    maxNativeZoom: 19
   });
 
   const layerSatelliteLabels = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png", {
     attribution: TILE_ATTRIBUTION,
     subdomains: "abcd",
-    maxZoom: 20
+    maxZoom: 20,
+    maxNativeZoom: 18
   });
 
   const layerSatellite = L.layerGroup([layerSatelliteImagery, layerSatelliteLabels]);
